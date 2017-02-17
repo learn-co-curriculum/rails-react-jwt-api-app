b101dce5a7c9cad21e13a4ca8b3e19b687451f14
Luke Ghenco <lukeghenco@gmail.com>
2017-02-17 11:23:01 -0500

    add User model with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217151538) do
+ActiveRecord::Schema.define(version: 20170217160802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
