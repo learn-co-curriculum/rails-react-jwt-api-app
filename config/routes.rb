5556bd1bcb47b0cf20d842f0baac7887af81314a
Luke Ghenco <lukeghenco@gmail.com>
2017-02-17 11:22:34 -0500

    add movie routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   post '/registrations/signup', to: 'registrations#signup'  
   post '/auth/login', to: 'auth#login'
 
+  resources :movies, only: [:index, :create, :show, :update, :destroy]
+
 end
