a8d640c1382ed08e3b74c9e16777ad1b32c3e0f4
Luke Ghenco <lukeghenco@gmail.com>
2017-02-17 11:22:13 -0500

    add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
   validates :email, :first_name, :last_name, :username, presence: true
   validates :email, :username, uniqueness: true
+
+  def full_name 
+    "#{self.first_name} #{self.last_name}"
+  end
 end
