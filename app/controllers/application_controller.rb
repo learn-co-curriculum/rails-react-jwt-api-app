0b798bee9714a4f88033031b9dc5b980b9cf71b9
Luke Ghenco <lukeghenco@gmail.com>
2017-02-17 11:20:54 -0500

    add authenticate_token and current_user helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
 
+  helper_method :authenticate_token!
+  helper_method :current_user
+
+  private 
+
+    def authenticate_token! 
+      if request.env["HTTP_AUTHORIZATION"]
+        token = request.env["HTTP_AUTHORIZATION"].split(" ")[1]
+        decoded = Auth.decode_token(token)
+        @user_id = decoded[0]["user_id"] if decoded
+        if !current_user || !decoded
+          render json: { message: "You are not authorized" } 
+        end
+      else 
+        render json: { message: "You must include a JWT Token!" }
+      end
+    end
+    
+    def current_user
+      @current_user ||= User.find_by(id: @user_id) if @user_id
+    end
+
 end
